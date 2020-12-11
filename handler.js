8678664b9495319f6ededd12de19c5e4f6d48200
jlmolina <j.l.molina@cgiar.org>
2020-12-11 18:30:41 -0500

    API nasa

--- handler.js
+++ handler.js
@@ -74,3 +74,30 @@ module.exports.directCall = async (event) => {
 };
 
 
+module.exports.nasaAPI = async (event) => {
+  let dataString = '';
+  
+  const response = await new Promise((resolve, reject) => {
+      const req = https.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", function(res) {
+        res.on('data', chunk => {
+          dataString += chunk;
+        });
+        res.on('end', () => {
+          resolve({
+              statusCode: 200,
+              body: JSON.stringify(JSON.parse(dataString), null, 4)
+          });
+        });
+      });
+      
+      req.on('error', (e) => {
+        reject({
+            statusCode: 500,
+            body: 'Something went wrong!'
+        });
+      });
+  });
+  return response;
+};
+
+
