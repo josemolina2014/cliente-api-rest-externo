e0dba7742e6271c36463d0173b6071905c4429d3
jlmolina <j.l.molina@cgiar.org>
2020-12-11 18:16:48 -0500

    llamado a ws externo

--- handler.js
+++ handler.js
@@ -1,18 +1,76 @@
 'use strict';
+const https = require('https');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+module.exports.invokeWS = async event => 
+  {
+    let dataString = '';
+    
+    const response = await new Promise((resolve, reject) => {
+        console.log('invokeWS --> '+event.pathParameters);
+        
+        const { queryStringParameters } = event;
+        if (!queryStringParameters || !queryStringParameters.name) {
+            console.log('BadRequest');
+            resolve({
+                statusCode: 400,
+                body: 'Please provide a Pokemon name!'
+            })
+        }
+        console.log(`https://pokeapi.co/api/v2/pokemon/${queryStringParameters.name}`);
+        const req = https.get(`https://pokeapi.co/api/v2/pokemon/${queryStringParameters.name}`, function(res) {
+            res.on('data', chunk => {
+                dataString += chunk;
+            });
+            res.on('end', () => {
+                console.log('OK');
+                console.log(JSON.stringify({ name: queryStringParameters.name }));
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify(JSON.parse(dataString), null, 4)
+            });
+          });
+        });
+        
+        req.on('error', (e) => {
+            console.log('ServerError');
+          reject({
+              statusCode: 500,
+              body: 'Something went wrong!'
+          });
+        });
+    });
+    
+    return response;
+  
+
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.directCall = async (event) => {
+  let dataString = '';
+  
+  const response = await new Promise((resolve, reject) => {
+      const req = https.get("https://pokeapi.co/api/v2/pokemon/ditto", function(res) {
+        res.on('data', chunk => {
+          dataString += chunk;
+        });
+        res.on('end', () => {
+          resolve({
+              statusCode: 200,
+              body: JSON.stringify(JSON.parse(dataString), null, 4)
+          });
+        });
+      });
+      
+      req.on('error', (e) => {
+        reject({
+            statusCode: 500,
+            body: 'Something went wrong!'
+        });
+      });
+  });
+  
+  return response;
+  //return sendResponse(constantes.SUCESSFULL_EXECUTION, response)
 };
+
+
